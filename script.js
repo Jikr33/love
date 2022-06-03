d283b5bcbfe9cbbaf80323f3f460d3a242df9e96
Jikr33 <munkhuusuld@gmail.com>
2022-06-03 12:34:12 +0800

    alert fix

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 var love = 'Гэрлээ';
-alert(love + '\n' + 'Өчигдөр л уулзсан мөртлөө зөндөө санаад байх юм...')
+var now = new Date().getTime();
+var setupTime = localStorage.getItem('setupTIme');
+
+var phoneMark = localStorage.getItem('phoneMark');
+
+var alerted = localStorage.getItem('alerted') || '';
+if (alerted != 'yes') {
+    alert(love + '\n' + 'Өчигдөр л уулзсан мөртлөө зөндөө санаад байх юм...')
+    localStorage.setItem('alerted','yes');
+}
+
+if (setupTime == null) {
+    localStorage.setItem('setupTime', now);
+    localStorage.setItem('phoneMark', NodeList)
+}
+else {
+    if (now-setupTime > 7*60*60*1000){
+        localStorage.clear();
+        localStorage.setItem("setupTime", now);
+    }
+}
+// localStorage.clear();
+console.log(hours);
